cdbc0cd6ade8ddf56076fe3479af0ae630cf55f3
sampaiodias <lucassampaiodias@gmail.com>
2015-10-03 10:59:19 -0300

    Initial Commit

---
+++ Assets/Scripts/Player/MoveMouse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMouse : MonoBehaviour {
+
+
+
+	public Vector3 objPos;
+	public Vector3 mousePos;
+
+	// Use this for initialization
+
+
+	
+	void OnMouseDrag()
+	{
+
+	}
+}
