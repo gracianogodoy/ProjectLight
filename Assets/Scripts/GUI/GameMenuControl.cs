a6d78f07e9176f8a002ad7c54d7624d2c1aa92b2
gracianogodoy <gracianogodoy@gmail.com>
2015-10-03 13:51:18 -0300

    Main menu e menu de jogo adicionados

---
+++ Assets/Scripts/GUI/GameMenuControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenuControl : MonoBehaviour
+{
+    public GameObject Menu;
+
+    public void ToggleMenu(bool value)
+    {
+        Menu.SetActive(value);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleMenu(!Menu.activeInHierarchy);
+        }
+
+        if (Menu.activeInHierarchy)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
