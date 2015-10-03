287caefe6d332c24e93e11ff2fca21aefcbf0e30
sampaiodias <lucassampaiodias@gmail.com>
2015-10-03 11:18:23 -0300

    MovimentoPlayer

--- Assets/Scripts/Player/MoveMouse.cs
+++ Assets/Scripts/Player/MoveMouse.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveMouse : MonoBehaviour {
-
-
-
-	public Vector3 objPos;
-	public Vector3 mousePos;
-
-	// Use this for initialization
-
-
+public class MoveMouse : MonoBehaviour 
+{
+	public float speed = 3.0f;
+	private Vector3 targetPos;
 	
-	void OnMouseDrag()
-	{
-
+	void Start() {
+		targetPos = transform.position;    
+	}
+	
+	void Update () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		targetPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+		targetPos = Camera.main.ScreenToWorldPoint(targetPos);
+		transform.position = Vector3.MoveTowards (transform.position, targetPos, speed * Time.deltaTime);
 	}
 }
