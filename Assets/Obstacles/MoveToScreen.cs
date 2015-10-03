ba9564d630fc64f6de6bdffd29dd03225d7fee49
andresalerno88 <andresalerno88@gmail.com>
2015-10-03 12:15:13 -0300

    Scripts de geração de obstaculos

---
+++ Assets/Obstacles/MoveToScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToScreen : MonoBehaviour {
+
+	public Vector3 Destination;
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveToDestination ();
+
+	}
+
+	void moveToDestination(){
+		Vector3 direction = Destination - transform.position;
+		direction.Normalize ();
+		transform.position += direction*Speed;
+		
+	}
+
+
+
+}
