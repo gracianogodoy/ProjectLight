e4c4e739e5b52f981152f6484c245b8244ef1028
sampaiodias <lucassampaiodias@gmail.com>
2015-10-03 15:07:26 -0300

    Movimentação Câmera INCOMPLETO

---
+++ Assets/Scripts/Utility/CameraMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour 
+{
+	public float speed = 1.0f;
+	private Vector3 targetPos;
+
+	public Transform playerTransform;
+	public Transform cameraTransform;
+
+	public float distance;
+	public float distancePosOriginal;
+	public float limiteX = 1;
+	public float limiteY = 1;
+	
+	void Start() {
+		//targetPos = transform.position;    
+	}
+	
+	void Update () {
+		distance = Vector3.Distance (
+			new Vector3(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z), 
+			new Vector3(cameraTransform.position.x, cameraTransform.position.y, cameraTransform.position.z));
+		if (distance < limiteX)
+		{
+			cameraTransform.position = new Vector3(
+				Mathf.Lerp(cameraTransform.position.x,playerTransform.position.x,Time.deltaTime*speed),
+				cameraTransform.position.y);
+		}
+
+
+		Debug.Log ("Distancia entre camera e player: " + distance);
+	}
+}
---
+++ Assets/Scripts/Utility/CameraMoving.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var target : Transform;
+var smooth : float = 5;
+var difX : float;
+var difY : float;
+var limiteX : float = 1;
+var limiteY : float = 1;
+var distance : float;
+function Update () {
+	//distance = Vector3.Distance(this.transform, target);
+	//difX = distance.x;
+	difY = target.position.y - transform.position.y;
+	if (difX < 0)
+	{
+		difX = -difX;
+	}
+	
+	if (difY < 0)
+	{
+		difY = -difY;
+	}
+	
+	
+	if (difX < limiteX)
+	{
+		this.transform.position.x = Mathf.Lerp(transform.position.x,target.position.x,Time.deltaTime*smooth);
+	}
+	
+    if (difY < limiteX)
+    {
+    	this.transform.position.y = Mathf.Lerp(transform.position.y,target.position.y,Time.deltaTime*smooth);
+    }
+   
+    
+    Debug.Log(""+ difX);
+}
+
