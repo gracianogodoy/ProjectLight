aee462cae4e648397f4632a2eaa1cefa4f90a69e
sampaiodias <lucassampaiodias@gmail.com>
2015-10-03 16:08:03 -0300

    Audio na Colisão

---
+++ Assets/Scripts/Player/Sfx.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sfx : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter()
+	{
+		GameObject cam = GameObject.Find("Main Camera");
+		CameraShake shake = (CameraShake)cam.GetComponent (typeof(CameraShake));
+		shake.shake = 0.5f;
+		AudioManager.Instance.Play ("ColisaoRuim");
+		Destroy (this.gameObject);
+
+	}
+}
---
+++ Assets/Scripts/Utility/AudioManager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public struct Sound
+{
+    public string Name;
+    public AudioClip Clip;
+    public float Volume;
+    public bool Loop;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] Sounds;
+
+    private List<AudioSource> _audioSources;
+
+    private static AudioManager _instance;
+
+    public static AudioManager Instance { get { return _instance; } }
+
+    // Use this for initialization
+    void Awake()
+    {
+        _audioSources = new List<AudioSource>();
+        _instance = this;
+    }
+
+    #region Play methods
+    public AudioSource Play(AudioClip sound, float volume, bool loop)
+    {
+        var source = getFreeAudioSource();
+
+        source.clip = sound;
+        source.volume = volume;
+        source.loop = loop;
+        source.playOnAwake = true;
+
+        source.Play();
+        return source;
+    }
+
+    //public AudioSource Play(string soundName, float volume, bool loop, bool playOnAwake)
+    //{
+    //    return Play(getSound(soundName), volume, loop, playOnAwake, false);
+    //}
+
+    //public AudioSource Play(string soundName, float volume, bool loop)
+    //{
+    //    return Play(getSound(soundName), volume, loop, false, false);
+    //}
+
+    //public AudioSource Play(string soundName, float volume)
+    //{
+    //    return Play(getSound(soundName), volume, false, false, false);
+    //}
+
+    public AudioSource Play(string soundName)
+    {
+        var sound = getSound(soundName);
+        return Play(sound.Clip, sound.Volume, sound.Loop);
+    }
+    #endregion
+
+    #region Helper methods
+    private Sound getSound(string name)
+    {
+        for (int i = 0; i < Sounds.Length; i++)
+        {
+            var sound = Sounds[i];
+
+            if (sound.Name == name)
+                return sound;
+        }
+
+        throw new Exception(string.Format("The sound {0} was not found.", name));
+    }
+
+    public AudioSource getFreeAudioSource()
+    {
+        for (int i = 0; i < _audioSources.Count; i++)
+        {
+            var audioSource = _audioSources[i];
+
+            if (!audioSource.isPlaying)
+            {
+                return audioSource;
+            }
+        }
+
+        var newAudioSource = createAudioSourceObject();
+        _audioSources.Add(newAudioSource);
+
+        return newAudioSource;
+    }
+
+    private AudioSource createAudioSourceObject()
+    {
+        GameObject obj = null;
+        obj = new GameObject("audio_source");
+        obj.transform.position = Instance.transform.position;
+        obj.transform.parent = Instance.transform;
+
+        AudioSource source = null;
+        source = obj.AddComponent<AudioSource>();
+
+        return source;
+    }
+    #endregion
+}
