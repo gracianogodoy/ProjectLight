b5e270c711f4afa49b61b1aa04fd09bfbdc32a34
andresalerno88 <andresalerno88@gmail.com>
2015-10-03 15:08:54 -0300

    Primeiros Prefabs

--- Assets/Scripts/Obstacles/ObstacleGeneration.cs
+++ Assets/Scripts/Obstacles/ObstacleGeneration.cs
@@ -11,6 +11,8 @@ public class ObstacleGeneration : MonoBehaviour {
 	public float RotateSpeed;
 	bool MovingNear = true;
 	public float AproximationSpeed;
+	public int TaxaDeRepetiçao;
+
 	// Use this for initialization
 	void Start () {
 		contadorTimerManqueba = 0;
@@ -20,9 +22,10 @@ public class ObstacleGeneration : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		contadorTimerManqueba++;
-		if (contadorTimerManqueba >= 60) {
-			CreateObstacle();
+		if (contadorTimerManqueba >= TaxaDeRepetiçao) {
+			CreateObstacle(1,0,0.4f,3);
 			contadorTimerManqueba=0;
+			TaxaDeRepetiçao += Random.Range(-1,2);
 		}
 		
 		ChangeDistance();
@@ -38,7 +41,7 @@ public class ObstacleGeneration : MonoBehaviour {
 			}
 		} else {
 			DistanceToOrigin += AproximationSpeed;
-			if (DistanceToOrigin >= 200) {
+			if (DistanceToOrigin >= 100) {
 				MovingNear = true;
 			}
 		
@@ -53,18 +56,30 @@ public class ObstacleGeneration : MonoBehaviour {
 
 
 	}
-
-	void CreateObstacle(){
-		GameObject g = (GameObject)Instantiate(Obstacle,Origin,Quaternion.identity);
-		g.GetComponent<MoveToScreen>().Destination = randomDestination();
-		
+	/// <summary>
+	/// Creates the obstacle.
+	/// </summary>
+	/// <param name="quantity">How many obstacles are generated.</param>
+	/// <param name="type">Type of the obstacles generated</param>
+	/// <param name="speed">Speed of the obstacles</param>
+	/// <param name="angle">How much the obstacle bends towards the screen</param>
+	void CreateObstacle(int quantity, int type,float speed,float angle){
+		for (int i = 0; i < quantity; i++) {
+			
+			GameObject g = (GameObject)Instantiate(Obstacle,Origin,Quaternion.identity);
+			MoveToScreen m = g.GetComponent<MoveToScreen> ();
+			m.Destination = randomDestination();
+			m.Speed = speed;
+			m.angle = angle;
+			
+		}
 		
 	}
 
 
 
 	Vector3 randomDestination(){
-		return new Vector3 (Random.Range(-dispersionRange,dispersionRange),Random.Range(-dispersionRange,dispersionRange),-11);
+		return new Vector3 (Random.Range(-dispersionRange,dispersionRange),Random.Range(-dispersionRange,dispersionRange),-25);
 	
 	
 	}
