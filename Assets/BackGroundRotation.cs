3f01a1978dc18973a1356dcb9ad245b98ea8b506
andresalerno88 <andresalerno88@gmail.com>
2015-10-04 15:43:39 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Assets/Prefabs/Obstacle1.prefab
    
    Merge branch 'master' of
    https://github.com/gracianogodoy/ProjectLight
    
    Conflicts:
    	Assets/Scenes/Model.meta
    	Assets/Scenes/Model/Materials.meta
    
    FundoGiratorio

---
+++ Assets/BackGroundRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundRotation : MonoBehaviour {
+
+	public float RotationAmmount = 0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(Vector3.up,RotationAmmount);
+	
+	}
+}
