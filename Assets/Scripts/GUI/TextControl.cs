f215350cde4b3e903c148ca16e5f0952da20a659
gracianogodoy <gracianogodoy@gmail.com>
2015-10-04 18:04:50 -0300

    ANiamtion

---
+++ Assets/Scripts/GUI/TextControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextControl : MonoBehaviour
+{
+    public Text textObject;
+    private string[] texts;
+
+    public void SetText(int textIndex)
+    {
+        if (textIndex == -1)
+        {
+            textObject.text = "";
+            return;
+        }
+
+        textObject.text = texts[textIndex];
+    }
+
+    public void Start()
+    {
+        texts = new string[]
+        {
+            Conversations.Fase1.dialogo1,
+            Conversations.Fase1.dialogo2,
+            Conversations.Fase1.dialogo3,
+
+            Conversations.Fase2.dialogo1,
+            Conversations.Fase2.dialogo2,
+            Conversations.Fase2.dialogo3,
+
+            Conversations.Fase3.dialogo1,
+            Conversations.Fase3.dialogo2,
+            Conversations.Fase3.dialogo3,
+            Conversations.Fase3.dialogo4,
+            Conversations.Fase3.dialogo5
+        };
+    }
+}
