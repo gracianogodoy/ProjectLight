c3d410a6e9054cbeb7392942a562c35a8bef40a6
sampaiodias <lucassampaiodias@gmail.com>
2015-10-04 12:06:45 -0300

    Vai gracianin!

--- Assets/Scripts/Utility/Stage.cs
+++ Assets/Scripts/Utility/Stage.cs
@@ -46,25 +46,25 @@ public class Stage : MonoBehaviour {
 		if (faseAtual == 1) {
 			alterarObs(1, 0, 0.2f, 3);
 			alterarLuz(1, 1, 0.3f, 3);
-			alterarTaxasDeRepeticao(30, 150);
+			alterarTaxasDeRepeticao(30, 150, 1);
 			sloMo(false);
 		}
 		if (faseAtual == 2) {
 			alterarObs(2, 0, 0.2f, 3);
 			alterarLuz(1, 2, 0.3f, 3);
-			alterarTaxasDeRepeticao(30, 150);
+			alterarTaxasDeRepeticao(30, 150, 2);
 			sloMo(false);
 		}
 		if (faseAtual == 3) {
 			alterarObs(3, 0, 0.2f, 3);
 			alterarLuz(1, 3, 0.3f, 3);
-			alterarTaxasDeRepeticao(30, 150);
+			alterarTaxasDeRepeticao(30, 150, 3);
 			sloMo(false);
 		}
 		if (faseAtual == 4) {
 			alterarObs(1, 0, 0.2f, 3);
 			alterarLuz(1, 0, 0.3f, 3);
-			alterarTaxasDeRepeticao(30, 150);
+			alterarTaxasDeRepeticao(30, 150, 0);
 			sloMo(true);
 		}
 
@@ -104,6 +104,16 @@ public class Stage : MonoBehaviour {
 
 		script.TaxaDeRepetiçaoObstaculos = obs;
 		script.TaxaDeRepetiçaoLuzes = luz;
+	}
+
+	void alterarTaxasDeRepeticao(int obs, int luz, int roam)
+	{
+		GameObject gerador = GameObject.Find ("ObstacleGenerator");
+		ObstacleGeneration script = (ObstacleGeneration)gerador.GetComponent (typeof(ObstacleGeneration));
+		
+		script.TaxaDeRepetiçaoObstaculos = obs;
+		script.TaxaDeRepetiçaoLuzes = luz;
+		script.quantidadeVagantes = roam;
 	}
 
 	void sloMo(bool ativado)
