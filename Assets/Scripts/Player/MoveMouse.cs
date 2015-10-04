0f072b1c70dad35e40d0be6c34f68402cd080c78
gracianogodoy <gracianogodoy@gmail.com>
2015-10-04 14:36:07 -0300

    Luquinha bichinha

--- Assets/Scripts/Player/MoveMouse.cs
+++ Assets/Scripts/Player/MoveMouse.cs
@@ -1,19 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveMouse : MonoBehaviour 
+public class MoveMouse : MonoBehaviour
 {
-	public float speed = 3.0f;
-	private Vector3 targetPos;
-	
-	void Start() {
-		targetPos = transform.position;    
-	}
-	
-	void Update () {
-		float distance = transform.position.z - Camera.main.transform.position.z;
-		targetPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-		targetPos = Camera.main.ScreenToWorldPoint(targetPos);
-		transform.position = Vector3.MoveTowards (transform.position, targetPos, speed * Time.deltaTime);
-	}
+    public float speed = 3.0f;
+    private Vector3 targetPos;
+
+    public Vector3 minCamera;
+    public Vector3 maxCamera;
+
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    void Update()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        var camera = Camera.main;
+        Vector2 mousePos = new Vector2(Mathf.Clamp(Input.mousePosition.x, 0, camera.pixelWidth),
+            Mathf.Clamp(Input.mousePosition.y, 0, camera.pixelHeight));
+        targetPos = new Vector3(mousePos.x, mousePos.y, distance);
+        targetPos = Camera.main.ScreenToWorldPoint(targetPos);
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+    }
 }
