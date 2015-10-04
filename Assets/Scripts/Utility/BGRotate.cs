eda5656269576399d9fdbb2a3ced5a39c87c894e
sybaristudios <sybaristudios@gmail.com>
2015-10-04 18:46:56 -0300

    BuracoNegro

---
+++ Assets/Scripts/Utility/BGRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGRotate : MonoBehaviour {
+    public float RotationSpeed = 0.1f;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.back,-RotationSpeed);
+	}
+}
